7f1ea578fd4eb81fdf481047ce0f80d6f9004e08
Toshiya <toshiya44@users.noreply.github.com>
2017-03-20 00:12:31 -0400

    Create break.user.js

---
+++ break.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name          Break
+// @description   Breaks long words.
+// @author        Toshiya
+// @version       0.0.1
+// @namespace     https://github.com/toshiya44
+// @include       http://example.com/*
+// @include       https://example.com/*
+// @grant         none
+// ==/UserScript==
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+addGlobalStyle('*{overflow-wrap: break-word !important;}');
